c05ac634bfba69d38f3e95c9ebd7c94c054e6781
Hoang Minh <kuminhuynhdoan@gmail.com>
2024-11-09 11:50:30 +0700

    asasd

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -23,7 +23,6 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
 
   def update
@@ -36,7 +35,6 @@ class BlogPostsController < ApplicationController
   end
   
   def destroy
-    @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy
     redirect_to root_path
   end
@@ -44,7 +42,7 @@ class BlogPostsController < ApplicationController
   private
 
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body, :published_at)
+    params.require(:blog_post).permit(:title, :content, :published_at)
   end
   
   def set_blog_post
